96fc72abb5a1635121bb4e41fe7e638aeed9d9ff
Rodrigo Rodrigues <rodrigomiguelsr2004@gmail.com>
2025-10-22 22:14:55 +0100

    fix: fixes missing .env variables

--- src/services/notification.ts
+++ src/services/notification.ts
@@ -1,4 +1,17 @@
+import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const messaging = getMessaging(app);
 
 export async function registerDevice() {
     console.log("backend-url: ", import.meta.env.VITE_BACKEND_URL);
@@ -34,6 +47,7 @@ export async function registerDevice() {
     });
 
     onMessage(messaging, (payload: any) => {
+        console.log("Message received. ", payload);
         new Notification(payload.notification?.title || "Notification", {
             body: payload.notification?.body,
             icon: "../assets/icon.png",
