d061841202f2600f61512f3cdf3e1f04ad2449c1
rmsr2004 <rodrigomiguelsr2004@gmail.com>
2026-07-26 15:58:15 +0100

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -12,7 +12,7 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    console.log("[firebase-messaging-sw.js] Received background message ", payload);
+    //console.log("[firebase-messaging-sw.js] Received background message ", payload);
     self.registration.showNotification(payload.notification.title, {
         body: payload.notification.body,
         icon: "/../src/assets/icon.png",
